03f04b781ca5af516f86ca30c943dc55b00bb94b
semnadtsat <hafidhakbar1@gmail.com>
2025-03-17 10:25:59 +0700

    change latest

--- fla.py
+++ fla.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "This is latest image"
+    return "This is latest image, and now it's changed"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=4090)
