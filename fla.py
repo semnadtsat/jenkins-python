a60049aeecc484f901dd33c484c21b2b94e0dfac
semnadtsat <hafidhakbar1@gmail.com>
2025-03-17 10:14:27 +0700

    fla changes

--- fla.py
+++ fla.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, The Last Mohicans"
+    return "Not something to proud about"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=4090)
